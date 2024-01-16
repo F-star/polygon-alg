1cccd0eeea9d70a5b47ec1db989b2d030d66946a
huanghao <hhbuhuiai@163.com>
2024-01-16 14:39:05 +0800

    feat: 只计算了交点的版本

--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,89 @@
-import { Point } from "./type"
+import { Point } from "./type";
+import "./util";
+import { getNewPolygon } from "./util";
 
-const canvas = document.querySelector('canvas')!
-const ctx = canvas.getContext('2d')!
+const canvas = document.querySelector("canvas")!;
+const ctx = canvas.getContext("2d")!;
 
-canvas.width = 600
-canvas.height = 600
+canvas.width = 600;
+canvas.height = 600;
 
 /**
  * @type {{x:number, y:number}[]}
  */
-const points: Point[] = []
+const points: Point[] = [];
 
 /**
  * @type {{x:number, y:number} | null}
  */
-let nextPoint: Point | null = null
+let nextPoint: Point | null = null;
 
 /**
- * 
- * @param {MouseEvent} e 
+ *
+ * @param {MouseEvent} e
  */
-const onMousedown = (e: MouseEvent) =>{
+const onMousedown = (e: MouseEvent) => {
   // 绘制点
-  const x = e.clientX
-  const y = e.clientY
-  points.push({x, y})
-  draw()
-}
+  const x = e.clientX;
+  const y = e.clientY;
+  points.push({ x, y });
+  draw();
+};
 
 /**
- * 
- * @param {MouseEvent} e 
+ *
+ * @param {MouseEvent} e
  */
 const onMousemove = (e: MouseEvent) => {
   // 暂时不需要
-  const x = e.clientX
-  const y = e.clientY
-  nextPoint = {x, y}
-  draw()
-}
+  const x = e.clientX;
+  const y = e.clientY;
+  nextPoint = { x, y };
+  draw();
+};
 
 const draw = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   // 绘制线
-  ctx.beginPath()
+  ctx.beginPath();
   for (let i = 0; i < points.length; i++) {
-    const p = points[i]
-    ctx.lineTo(p.x, p.y)
+    const p = points[i];
+    ctx.lineTo(p.x, p.y);
   }
   if (nextPoint) {
-    ctx.lineTo(nextPoint.x, nextPoint.y)
+    ctx.lineTo(nextPoint.x, nextPoint.y);
   }
-  ctx.closePath()
-  ctx.stroke()
+  ctx.closePath();
+  ctx.stroke();
 
   for (let i = 0; i < points.length; i++) {
-    const p = points[i]
-    drawPoint(p)
+    const p = points[i];
+    drawPoint(p);
   }
   if (nextPoint) {
-    drawPoint(nextPoint)
+    drawPoint(nextPoint);
   }
+  // 计算交点
+  if (points.length + 1 >= 3) {
+    const intersectionPoints = getNewPolygon([...points, nextPoint!], []);
+    for (const pt of intersectionPoints) {
+      drawPoint(pt, "#f04");
+    }
+  }
+};
 
-}
-
-const drawPoint = ({x, y}) => {
+const drawPoint = ({ x, y }: Point, color?: string) => {
+  ctx.save();
+  if (color) {
+    ctx.fillStyle = color;
+  }
   ctx.beginPath();
   ctx.arc(x, y, 2, 0, Math.PI * 2);
   ctx.fill();
   ctx.closePath();
-}
+  ctx.restore();
+};
 
-
-canvas.addEventListener('mousedown', onMousedown)
-canvas.addEventListener('mousemove', onMousemove)
+canvas.addEventListener("mousedown", onMousedown);
+canvas.addEventListener("mousemove", onMousemove);
--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,112 @@
+type Point = { x: number; y: number };
+type Edge = [number, number];
+type Polygon = { [key: number]: { point: Point; edge: number[] } };
 
-/**
- * 计算多边形所有交点
- */
+const getLineSegIntersection = (
+  p1: Point,
+  p2: Point,
+  p3: Point,
+  p4: Point
+): Point | null => {
+  const { x: x1, y: y1 } = p1;
+  const { x: x2, y: y2 } = p2;
+  const { x: x3, y: y3 } = p3;
+  const { x: x4, y: y4 } = p4;
 
-// 计算图。。
+  const a = y2 - y1;
+  const b = x1 - x2;
+  const c = x1 * y2 - x2 * y1;
 
+  const d = y4 - y3;
+  const e = x3 - x4;
+  const f = x3 * y4 - x4 * y3;
 
+  // 计算分母
+  const denominator = a * e - b * d;
 
-const getGraphFromPolygon = (points: Point[]) => {
-  // 1. 计算每条边和其他边的交点
-  // 
+  // 判断分母是否为 0（代表平行）
+  if (Math.abs(denominator) < 0.000000001) {
+    // 这里有个特殊的重叠但只有一个交点的情况，可以考虑处理一下
+    return null;
+  }
+
+  const px = (c * e - f * b) / denominator;
+  const py = (a * f - c * d) / denominator;
+
+  // 判断交点是否在两个线段上
+  if (
+    px >= Math.min(x1, x2) &&
+    px <= Math.max(x1, x2) &&
+    py >= Math.min(y1, y2) &&
+    py <= Math.max(y1, y2) &&
+    px >= Math.min(x3, x4) &&
+    px <= Math.max(x3, x4) &&
+    py >= Math.min(y3, y4) &&
+    py <= Math.max(y3, y4)
+  ) {
+    return { x: px, y: py };
+  }
+
+  return null;
+};
+
+export function getNewPolygon(points: Point[], adjList: number[][]) {
+  if (points.length < 3) {
+    console.warn("点至少得 3 个");
+    return [];
+  }
+  // 0. 补一个点。
+  // points = [...points, { ...points[0] }];
+
+  const intersectionPoints: Point[] = [];
+  const newAdjList = JSON.parse(JSON.stringify(adjList)); // 拷贝一份
+
+  // 1. 计算交点
+  for (let i = 0; i < points.length - 1; i++) {
+    const line1Start = points[i];
+    const line1End = points[i + 1];
+
+    let j = i + 2;
+    for (; j < points.length; j++) {
+      if (i === 0 && (j + 1) % points.length === 0) {
+        // 两个是连续的线，且其中一个位置为 0，没有计算交点的意义
+        continue;
+      }
+
+      console.log(`${i}-${i + 1}, ${j}-${j + 1}`);
+      const line2Start = points[j];
+      const line2End = points[(j + 1) % points.length];
+      const intersectionPoint = getLineSegIntersection(
+        line1Start,
+        line1End,
+        line2Start,
+        line2End
+      );
+      if (intersectionPoint) {
+        // 更新邻接表
+        intersectionPoints.push(intersectionPoint);
+      }
+    }
+  }
+
+  return intersectionPoints;
 }
+
+// 多边形
+const points: Point[] = [
+  { x: 0, y: 0 },
+  { x: 6, y: 0 },
+  { x: 0, y: 10 },
+  { x: 6, y: 10 },
+];
+
+// 邻接表
+const adjList = [
+  [2, 4],
+  [1, 3],
+  [2, 4],
+  [1, 3],
+];
+
+const data = getNewPolygon(points, []);
+console.log(data);
